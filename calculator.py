459f335dd81305a21d98c270822d2bf8120cad7d
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-02-24 22:22:55 +0500

    added calculation functios

--- calculator.py
+++ calculator.py
@@ -1,4 +1,49 @@
 def add(a, b):
   result = a + b
   print(str(a) + "+" + str(b) + "=" + str(result))
-add(1,2)
+
+def sub(a,b):
+  result = a - b
+  print(str(a) + "-" + str(b) + "=" + str(result))
+
+def mul(a,b):
+  result = a * b
+  print(str(a) + "*" + str(b) + "=" + str(result))
+
+def div(a,b):
+  result = a / b
+  print(str(a) + "/" + str(b) + "=" + str(result))
+
+while True:
+    
+  print("A. Addtion")
+  print("B. Subtraction")
+  print("C. Multiplication")
+  print("D. Division")
+  print("E. Exit")
+
+  choise = input("input your choice: ")
+
+  if choise == "a" or choise == "A":
+    print("Addition")
+    a = int(input("input first number:"))
+    b = int(input("input second number:"))
+    add(a,b)
+  elif choise == "b" or choise == "B":
+    print("Subtraction")
+    a = int(input("input first number:"))
+    b = int(input("input second number:"))
+    sub(a,b)
+  elif choise == "c" or choise == "C":
+    print("Multiplication")
+    a = int(input("input first number:"))
+    b = int(input("input second number:"))
+    mul(a,b)
+  elif choise == "d" or choise == "D":
+    print("Division")
+    a = int(input("input first number:"))
+    b = int(input("input second number:"))
+    div(a,b)
+  elif choise == "e" or choise == "E":
+    print("program ended")
+    quit()
