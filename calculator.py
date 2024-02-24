da9fae4bd4eae105bc206d916b18dbdfecc47139
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-02-24 18:54:47 +0500

    added replacement function

---
+++ calculator.py
@@ -0,0 +1,4 @@
+def add(a, b):
+  result = a + b
+  print(str(a) + "+" + str(b) + "=" + str(result))
+add(1,2)
---
+++ replacement.py
@@ -0,0 +1,7 @@
+def replacement_word():
+  str = "Hi i am ali, how are you bro!"
+  word_to_replace = input("Enter the word to replace: ")
+  word_to_replacement = input("Enter the word to replacement: ")
+  print(str.replace(word_to_replace, word_to_replacement))
+
+replacement_word()
